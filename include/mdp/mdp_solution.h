138e7a5ade6e14483c57e477ef9693058a3d6d81
Jaime <alu0101329093@ull.edu.es>
2022-05-10 04:13:49 +0100

    Added mdp solution output

--- include/mdp/mdp_solution.h
+++ include/mdp/mdp_solution.h
@@ -1,6 +1,7 @@
 #ifndef P8MDP_MDP_MDP_SOLUTION_H_
 #define P8MDP_MDP_MDP_SOLUTION_H_
 
+#include <iostream>
 #include <set>
 #include <vector>
 
@@ -17,6 +18,21 @@ class MdpSolution {
     elements_set_ = elements_set;
   }
   inline std::size_t GetElementsAmount() const { return elements_amount_; }
+
+  friend std::ostream& operator<<(std::ostream& os,
+                                  const MdpSolution& solution) {
+    os << "Elements amount: " << solution.elements_amount_ << "\n";
+    os << "Elements:\n";
+    std::size_t index{};
+    for (const auto& element : solution.elements_set_) {
+      std::cout << index++ << ": ";
+      for (std::size_t i = 0; i < element.size(); ++i) {
+        std::cout << element[i] << (i == element.size() - 1 ? "\n" : ", ");
+      }
+    }
+
+    return os;
+  }
 
  private:
   ElementsSet elements_set_;
